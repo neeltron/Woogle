977db6cf79f289986688f4bf9993507b805c3e33
Neel Adwani <nprosofficial@gmail.com>
2021-08-01 12:37:20 +0000

    push data to replit db

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, make_response, redirect, url_for
 from flask_socketio import SocketIO
+from replit import db
 
 app = Flask(
   __name__,
@@ -15,54 +16,25 @@ def index():
   return render_template('index.html')
 
 
-
-# Creating different routes
-@app.route('/second')
-def second():
-  return "I'm on a separate route"
-
-
-
-# HTTP Methods
-@app.route('/requesthttp', methods=['GET', 'POST'])
-def requesthttp():
-  if request.method == 'POST':
-    return "Auth here"
-  else:
-    return "Ask for creds here"
+@app.route('/push', methods= ["GET", "POST"])
+def push():
+  if request.method == "POST":
+    user = request.form.get('name')
+    i1 = request.form.get('i1')
+    i2 = request.form.get('i2')
+    i3 = request.form.get('i3')
+    lst = [i1, i2, i3]
+    db[user] = lst
+    print(user, i1, i2, i3)
 
 
 
 # File Uploads (needs an HTML Form)
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
-  if request.method == 'GET':
-    file = request.files['filename']
-    file.save('uploads/upload.txt')
-
-
-
-# Reading Cookies 🍪
-@app.route('/readcookie')
-def readcookie():
-  cookie = request.cookies.get('cookie')
-  return cookie
-
-
-
-# Storing Cookies
-@app.route('/storecookie')
-def storecookie():
-  response = make_response(render_template(index.html))
-  response.set_cookie('cookie', 'whatever')
-  return response
-  
-
-
-# Redirects
-@app.route('/redirect')
-def redirec():
-  return redirect(url_for('index'))
+# @app.route('/upload', methods=['GET', 'POST'])
+# def upload_file():
+#   if request.method == 'GET':
+#     file = request.files['filename']
+#     file.save('uploads/upload.txt')
 
 
 
