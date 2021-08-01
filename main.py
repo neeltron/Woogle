e2fc825796dc943f33fd5a51293ca1e8f6ff1fb7
Neel Adwani <nprosofficial@gmail.com>
2021-08-01 11:53:19 +0000

    added and debugged sockets py script

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from flask import Flask, render_template, request, make_response, redirect, url_for
-
-
+from flask_socketio import SocketIO
 
 app = Flask(
   __name__,
   template_folder='templates',
   static_folder='static'
 )
+app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+socketio = SocketIO(app)
 
 # Index page and Rendering Basic Templates
-@app.route('/')
+@app.route('/', methods= ["GET", "POST"])
 def index():
   return render_template('index.html')
 
@@ -65,10 +66,15 @@ def redirec():
 
 
 
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
+
+
+
 if __name__ == '__main__':
-  # Run the Flask app
-  app.run(
-	host='0.0.0.0',
-	debug=True,
-	port=8080
-  )
+  socketio.run(app, host = '0.0.0.0', debug=True, port = 8080)
